70a551cbae42febe7971d6482735d7c14bd7c35c
JPlante9117 <jacrplante@gmail.com>
2020-03-23 12:37:31 -0400

    added button to change display text

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [outputText, setOutputText] = useState('Hey!')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText("You've pressed a button")} />
     </View>
   );
 }
